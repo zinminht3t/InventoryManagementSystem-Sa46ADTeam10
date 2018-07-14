f71decb22e8a5bef5b1ff8c39638b70460976592
Zin Min Htet <zinminht3t@gmail.com>
2018-07-14 21:07:28 +0800

    Added Item API

---
+++ LUSSISADTeam10API/Controllers/ItemController.cs
@@ -0,0 +1,111 @@
+﻿using LUSSISADTeam10API.Constants;
+using LUSSISADTeam10API.Models.APIModels;
+using LUSSISADTeam10API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LUSSISADTeam10API.Controllers
+{
+    // to allow access only by login user
+    [Authorize]
+    public class ItemController : ApiController
+    {
+        // to show item list
+        [HttpGet]
+        [Route("api/items")]
+        public IHttpActionResult GetAllItems()
+        {
+            // declare and initialize error variable to accept the error from Repo
+            string error = "";
+
+            // get the list from itemrepo and will insert the error if there is one
+            List<ItemModel> ims = ItemRepo.GetAllItems(out error);
+
+            // if the erorr is not blank or the item list is null
+            if (error != "" || ims == null)
+            {
+                // if the error is 404
+                if (error == ConError.Status.NOTFOUND)
+                    return Content(HttpStatusCode.NotFound, "Items Not Found");
+                // if the error is other one
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            // if there is no error
+            return Ok(ims);
+
+        }
+
+        // to get item by item id
+        [HttpGet]
+        [Route("api/item/{itemid}")]
+        public IHttpActionResult GetItemByItemid(int itemid)
+        {
+            string error = "";
+            ItemModel im = ItemRepo.GetItemByItemid(itemid, out error);
+            if (error != "" || im == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "Item Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(im);
+        }
+
+        // to get item by category id
+        [HttpGet]
+        [Route("api/item/{itemid}")]
+        public IHttpActionResult GetItemByCatid(int catid)
+        {
+            string error = "";
+            ItemModel im = ItemRepo.GetItemByCatid(catid, out error);
+            if (error != "" || im == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "Item Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(im);
+        }
+
+        // to update item
+        [HttpPost]
+        [Route("api/item/update")]
+        public IHttpActionResult UpdateItem(ItemModel item)
+        {
+            string error = "";
+            ItemModel im = ItemRepo.UpdateItem(item, out error);
+            if (error != "" || im == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "Item Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(im);
+        }
+
+        // to create new item
+        [HttpPost]
+        [Route("api/item/create")]
+        public IHttpActionResult CreateItem(ItemModel item)
+        {
+            string error = "";
+            ItemModel im = ItemRepo.CreateItem(item, out error);
+            if (error != "" || im == null)
+            {
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(im);
+        }
+
+    }
+}
---
+++ LUSSISADTeam10API/Models/APIModels/ItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSISADTeam10API.Models.APIModels
+{
+    public class ItemModel
+    {
+        public ItemModel(int itemid, int catid, string name, string description, string uom)
+        {
+            this.itemid = itemid;
+            this.catid = catid;
+            this.name = name;
+            this.description = description;
+            this.uom = uom;
+        }
+        public ItemModel():this(0, 0, "", "", "") { }
+        public int itemid { get; set; }
+        public int catid { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string uom { get; set; }
+    }
+}
