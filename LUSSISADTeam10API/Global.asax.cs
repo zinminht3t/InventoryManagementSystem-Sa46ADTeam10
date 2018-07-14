dc62d90ee6b95cb0d978cd4d342b707dc232b718
e0284030 <thetaungzaw1995@gmail.com>
2018-07-14 17:02:32 +0800

    Added Comment

--- LUSSISADTeam10API/Global.asax.cs
+++ LUSSISADTeam10API/Global.asax.cs
@@ -14,7 +14,7 @@ namespace LUSSISADTeam10API
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
+            // Code that runs on application startup ---
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
