7947a0212bdd52c4739550742620cf8d68415b76
Zin Min Htet <zinminht3t@gmail.com>
2018-07-15 16:06:35 +0800

    Setup for Web App to consume API done!

---
+++ LUSSISADTeam10Web/Constants/ConLockerCollectionPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSISADTeam10Web.Constants
+{
+    public class ConLockerCollectionPoint
+    {
+        public static class Active
+        {
+            public const int AVAILABLE = 1;
+            public const int NOTAVAILABLE = 0;
+        }
+    }
+}
