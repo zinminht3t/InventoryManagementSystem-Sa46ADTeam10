61a12a33346e5dc0fd57747cc9534290f2f2770e
e0284030 <thetaungzaw1995@gmail.com>
2018-07-15 13:27:52 +0800

    Added Collection Point

---
+++ LUSSISADTeam10API/Controllers/CollectionPointController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LUSSISADTeam10API.Constants;
+using LUSSISADTeam10API.Models.APIModels;
+using LUSSISADTeam10API.Repositories;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LUSSISADTeam10API.Controllers
+{ 
+        // to allow access only by login user
+        [Authorize]
+        public class CollectionPointController : ApiController
+        {
+            // to show collectionpoint list
+            [HttpGet]
+            [Route("api/collectionpoints")]
+            public IHttpActionResult GetAllCollectionPoints()
+            {
+                // declare and initialize error variable to accept the error from Repo
+                string error = "";
+
+                // get the list from collectionpointrepo and will insert the error if there is one
+                List<CollectionPointModel> cpm = CollectionPointRepo.GetAllCollectionPoint(out error);
+
+                // if the erorr is not blank or the collectionpoint list is null
+                if (error != "" || cpm == null)
+                {
+                    // if the error is 404
+                    if (error == ConError.Status.NOTFOUND)
+                        return Content(HttpStatusCode.NotFound, "CollectionsPoints Not Found");
+                    // if the error is other one
+                    return Content(HttpStatusCode.BadRequest, error);
+                }
+                // if there is no error
+                return Ok(cpm);
+
+            }
+        // to get collectoinpoint by collectionpoint id
+        [HttpGet]
+        [Route("api/collectionpoint/{cpid}")]
+        public IHttpActionResult GetCollectionPointByCpid(int cpid)
+        {
+            string error = "";
+            CollectionPointModel cpm = CollectionPointRepo.GetCollectionPointByCpid(cpid, out error);
+            if (error != "" || cpm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "CollectionPoint Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(cpm);
+        }
+        // to get department by requisition id
+        [HttpGet]
+        [Route("api/collectionpoint/requisition/{reqid}")]
+        public IHttpActionResult GetCollectionPointByReqid(int reqid)
+        {
+            string error = "";
+            CollectionPointModel cpm = CollectionPointRepo.GetCollectionPointByReqid(reqid, out error);
+            if (error != "" || cpm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "Requisition Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(cpm);
+        }
+        // to get collectionpoint by department id
+        [HttpGet]
+        [Route("api/collectionpoint/department/{deptid}")]
+        public IHttpActionResult GetCollectionPointByDeptid(int deptid)
+        {
+            string error = "";
+            CollectionPointModel cpm = CollectionPointRepo.GetCollectionPointByDeptid(deptid, out error);
+            if (error != "" || cpm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "Department Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(cpm);
+        }
+        // to get collectionpoint by locker id
+        [HttpGet]
+        [Route("api/collectionpoint/lockercollectionpoint/{lockerid}")]
+        public IHttpActionResult GetCollectionPointByLockerid(int lockerid)
+        {
+            string error = "";
+            CollectionPointModel cpm = CollectionPointRepo.GetCollectionPointByLockerid(lockerid, out error);
+            if (error != "" || cpm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "Locker Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(cpm);
+        }
+        // to update collectionpoint
+        [HttpPost]
+        [Route("api/collectionpoint/update")]
+        public IHttpActionResult UpdateCollectionPoint(CollectionPointModel cp)
+        {
+            string error = "";
+            CollectionPointModel cpm=CollectionPointRepo.UpdateCollectionPoint(cp, out error);
+            if (error != "" || cpm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "CollectionPoint Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(cpm);
+        }
+
+        // to create new collectionpoint
+        [HttpPost]
+        [Route("api/collectionpoint/create")]
+        public IHttpActionResult CreateCollectionPoint(CollectionPointModel cp)
+        {
+            string error = "";
+            CollectionPointModel cpm = CollectionPointRepo.CreateCollectionPoint(cp, out error);
+            if (error != "" || cpm == null)
+            {
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(cpm);
+        }
+    }
+}
---
+++ LUSSISADTeam10API/Repositories/CollectionPointRepo.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using LUSSISADTeam10API.Constants;
+using LUSSISADTeam10API.Models.APIModels;
+using LUSSISADTeam10API.Models.DBModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace LUSSISADTeam10API.Repositories
+{
+    public static class CollectionPointRepo
+    {
+        // entites used only by Get Methods
+        private static LUSSISEntities entities = new LUSSISEntities();
+
+        // Convert From Auto Generated DB Model to APIModel
+        private static CollectionPointModel CovertDBCptoAPICp(collectionpoint cp)
+        {
+            CollectionPointModel cpm = new CollectionPointModel(cp.cpid, cp.cpname, cp.cplocation);
+            return cpm;
+        }
+
+
+        // Get the list of all collectionpoints and will return with error if there is one.
+        public static List<CollectionPointModel> GetAllCollectionPoint(out string error)
+        {
+            // Initializing the error variable to return only blank if there is no error
+            error = "";
+            List<CollectionPointModel> cpm = new List<CollectionPointModel>();
+            try
+            {
+                // get collection list from database
+                List<collectionpoint> cps = entities.collectionpoints.ToList<collectionpoint>();
+
+                // convert the DB Model list to API Model list
+                foreach (collectionpoint cp in cps)
+                { 
+                    cpm.Add(CovertDBCptoAPICp(cp));
+                }
+            }
+
+            // if collectionpoint not found, will throw NOTFOUND exception
+            catch (NullReferenceException)
+            {
+                // if there is NULL Exception error, error will be 404
+                error = ConError.Status.NOTFOUND;
+            }
+
+            catch (Exception e)
+            {
+                // for other exceptions
+                error = e.Message;
+            }
+
+            //returning the list
+            return cpm;
+        }
+        public static CollectionPointModel GetCollectionPointByCpid(int cpid, out string error)
+        {
+            error = "";
+
+            collectionpoint cp = new collectionpoint();
+
+            CollectionPointModel cpm = new CollectionPointModel();
+            try
+            {
+                cp = entities.collectionpoints.Where(p => p.cpid == cpid).FirstOrDefault<collectionpoint>();
+                cpm = CovertDBCptoAPICp(cp);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return cpm;
+        }
+        public static CollectionPointModel GetCollectionPointByReqid(int reqid, out string error)
+        {
+            error = "";
+            collectionpoint cp = new collectionpoint();
+            requisition req = new requisition();
+            CollectionPointModel cpm = new CollectionPointModel();
+
+            try
+            {
+                req = entities.requisitions.Where(p => p.reqid == reqid).FirstOrDefault<requisition>();
+                cp = req.collectionpoint;
+                cpm = CovertDBCptoAPICp(cp);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return cpm;
+        }
+        public static CollectionPointModel GetCollectionPointByDeptid(int deptid, out string error)
+        {
+            error = "";
+
+            collectionpoint cp = new collectionpoint();
+            departmentcollectionpoint dcp = new departmentcollectionpoint();
+            CollectionPointModel cpm = new CollectionPointModel();
+            try
+            {
+                dcp = entities.departmentcollectionpoints.Where(p => p.deptid == deptid).FirstOrDefault<departmentcollectionpoint>();
+                cp = dcp.collectionpoint;
+                cpm = CovertDBCptoAPICp(cp);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return cpm;
+        }
+        public static CollectionPointModel GetCollectionPointByLockerid(int lockerid, out string error)
+        {
+            error = "";
+
+            collectionpoint cp = new collectionpoint();
+            lockercollectionpoint lcp = new lockercollectionpoint();
+            CollectionPointModel cpm = new CollectionPointModel();
+            try
+            {
+                lcp = entities.lockercollectionpoints.Where(p => p.lockerid == lockerid).FirstOrDefault<lockercollectionpoint>();
+                cp = lcp.collectionpoint;
+                cpm = CovertDBCptoAPICp(cp);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return cpm;
+        }
+        public static CollectionPointModel UpdateCollectionPoint(CollectionPointModel cpm, out string error)
+        {
+            error = "";
+            // declare and initialize new LUSSISEntities to perform update
+            LUSSISEntities entities = new LUSSISEntities();
+            collectionpoint cp = new collectionpoint();
+            try
+            {
+                // finding the collectionpoint object using CollectionPoint API model
+                cp = entities.collectionpoints.Where(p => p.cpid == cpm.cpid).First<collectionpoint>();
+
+                // transfering data from API model to DB Model
+                cp.cpname = cpm.cpname;
+                cp.cplocation = cpm.cplocation;
+
+              
+
+                // saving the update
+                entities.SaveChanges();
+
+                // return the updated model 
+                cpm = CovertDBCptoAPICp(cp);
+               
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return cpm;
+        }
+        public static CollectionPointModel CreateCollectionPoint(CollectionPointModel cpm, out string error)
+        {
+            error = "";
+            LUSSISEntities entities = new LUSSISEntities();
+            collectionpoint cp = new collectionpoint();
+            try
+            {
+                cp.cpname = cpm.cpname;
+                cp.cplocation = cpm.cplocation;
+                
+                cp = entities.collectionpoints.Add(cp);
+                entities.SaveChanges();
+                cpm = CovertDBCptoAPICp(cp);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return cpm;
+        }
+    }
+    }
+
