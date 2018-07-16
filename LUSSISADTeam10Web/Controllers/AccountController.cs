47789fe838203fcf0e6d7bd8b0553761598cf9a4
Chit Su Shine <e0284025@u.nus.edu>
2018-07-16 09:16:52 +0800

    Delete Error

--- LUSSISADTeam10Web/Controllers/AccountController.cs
+++ LUSSISADTeam10Web/Controllers/AccountController.cs
@@ -76,18 +76,6 @@ namespace LUSSISADTeam10Web.Controllers
             return View();
 
 
-            string userName = (string)Session["UserName"];
-            string[] userRoles = (string[])Session["UserRoles"];
-
-            ClaimsIdentity identity = new ClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie);
-
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, userName));
-
-            userRoles.ToList().ForEach((role) => identity.AddClaim(new Claim(ClaimTypes.Role, role)));
-
-            identity.AddClaim(new Claim(ClaimTypes.Name, userName));
-
-            AuthenticationManager.SignIn(identity);
         }
     }
 }
