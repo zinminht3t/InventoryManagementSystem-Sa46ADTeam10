dab776c036009ecddab940062bb1050e4629400c
e0284030 <thetaungzaw1995@gmail.com>
2018-07-16 13:43:11 +0800

    All user Dashboard added

--- LUSSISADTeam10Web/Controllers/EmployeeController.cs
+++ LUSSISADTeam10Web/Controllers/EmployeeController.cs
@@ -11,7 +11,7 @@ namespace LUSSISADTeam10Web.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            return View("EmpDashboard");
         }
     }
 }
