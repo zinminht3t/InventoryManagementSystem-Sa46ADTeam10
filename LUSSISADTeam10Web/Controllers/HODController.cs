fdd9a39b823f8b9c68380b0d2abf38bf08f34a16
Zin Min Htet <zinminht3t@gmail.com>
2018-07-17 19:40:09 +0800

    HOD Sample

--- LUSSISADTeam10Web/Controllers/HODController.cs
+++ LUSSISADTeam10Web/Controllers/HODController.cs
@@ -74,6 +74,21 @@ namespace LUSSISADTeam10Web.Controllers
 
         // Start ZMH
 
+        public ActionResult Requisitions()
+        {
+            string token = "QvYxJISfdbvAZ5L6tBTI52hMTZ_Gowj7LIv57m3d87ej6zhPP96f2qxaSio9S5_hWKbd8XJFEVmbdxBlDTfiXXvYgqyEmQkFQD8lQNqXovK2jh7mMUphmoQrdqnJekFmSguzG7kpYHC36zQla4WWDx7urd71jQ2nHOcnOKPgBvkEidsLXQ9qsRx2hW7kxnvI7vuYZk2oGEfk_EZeHc5O_RFRek3Pp9GhJSOO05yznpnQfBK4j81898XuvzVYdsYTb_FhnTNAyzjrtlPa4rBf0FZGhqFN7Q9ja13dwNoX8PV2x_eU2VN-dDG_HxFpMJkCA15SKK73mjk3oW4HQwT3SXkYgFRjPFlyT-MYD8Cdr4g";
+            List<RequisitionModel> rms = APIRequisition.GetRequisitionByDepid(1, token, out string error);
+            return View(rms);
+        }
+
+        public ActionResult RequisitionDetail(int id)
+        {
+            string token = "QvYxJISfdbvAZ5L6tBTI52hMTZ_Gowj7LIv57m3d87ej6zhPP96f2qxaSio9S5_hWKbd8XJFEVmbdxBlDTfiXXvYgqyEmQkFQD8lQNqXovK2jh7mMUphmoQrdqnJekFmSguzG7kpYHC36zQla4WWDx7urd71jQ2nHOcnOKPgBvkEidsLXQ9qsRx2hW7kxnvI7vuYZk2oGEfk_EZeHc5O_RFRek3Pp9GhJSOO05yznpnQfBK4j81898XuvzVYdsYTb_FhnTNAyzjrtlPa4rBf0FZGhqFN7Q9ja13dwNoX8PV2x_eU2VN-dDG_HxFpMJkCA15SKK73mjk3oW4HQwT3SXkYgFRjPFlyT-MYD8Cdr4g";
+            RequisitionModel rms = APIRequisition.GetRequisitionByReqid(id, token, out string error);
+            return View(rms);
+        }
+
+
         // End ZMH
 
     }
