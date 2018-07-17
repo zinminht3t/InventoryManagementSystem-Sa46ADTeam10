94eb54582a2f3fdee129146200e91acde07bd737
Zin Min Htet <zinminht3t@gmail.com>
2018-07-17 14:43:46 +0800

    Latest

--- LUSSISADTeam10Web/Controllers/HomeController.cs
+++ LUSSISADTeam10Web/Controllers/HomeController.cs
@@ -35,8 +35,7 @@ namespace LUSSISADTeam10Web.Controllers
         public ActionResult Haha()
         {
             string error = "";
-
-            string token = (string)Session["token"];
+            string token = "QvYxJISfdbvAZ5L6tBTI52hMTZ_Gowj7LIv57m3d87ej6zhPP96f2qxaSio9S5_hWKbd8XJFEVmbdxBlDTfiXXvYgqyEmQkFQD8lQNqXovK2jh7mMUphmoQrdqnJekFmSguzG7kpYHC36zQla4WWDx7urd71jQ2nHOcnOKPgBvkEidsLXQ9qsRx2hW7kxnvI7vuYZk2oGEfk_EZeHc5O_RFRek3Pp9GhJSOO05yznpnQfBK4j81898XuvzVYdsYTb_FhnTNAyzjrtlPa4rBf0FZGhqFN7Q9ja13dwNoX8PV2x_eU2VN-dDG_HxFpMJkCA15SKK73mjk3oW4HQwT3SXkYgFRjPFlyT-MYD8Cdr4g";
             List<DepartmentModel> dms = APIDepartment.GetAllDepartments(token, out error);
             return View(dms);
         }
