fa9527ca720ce5bc142d0105b2898991eef615a2
Khin Yadana Phyo <bakafool12@gmail.com>
2018-07-19 12:59:20 +0800

    noti

--- LUSSISADTeam10API/Models/APIModels/NotificationModel.cs
+++ LUSSISADTeam10API/Models/APIModels/NotificationModel.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
 
 namespace LUSSISADTeam10API.Models.APIModels
 {
     public class NotificationModel
     {
-    }
+        public NotificationModel(int notiID, DateTime datetime, int deptid, int role, string title, string remark, int isread)
+        {
+            NotiID = notiID;
+            Datetime = datetime;
+            Deptid = deptid;
+            Role = role;
+            Title = title;
+            Remark = remark;
+            Isread = isread;
+        }
+
+        public NotificationModel():this(0, new DateTime(), 0, 0, "", "", 0) { }
+
+        public int NotiID { get; set; }
+        public DateTime  Datetime{ get; set; }
+
+        public int Deptid  { get; set; }
+
+        public int Role { get; set; }
+
+        public string Title { get; set; }
+
+        public string Remark { get; set; }
+
+        public int Isread { get; set; }
+
+}
 }
