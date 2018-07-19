5a105894bd6c6d524161b96a9a432d859ba1eacb
Zin Min Htet <zinminht3t@gmail.com>
2018-07-19 14:23:14 +0800

    Inventory Transaction API

---
+++ LUSSISADTeam10API/Controllers/InventoryTransactionController.cs
@@ -0,0 +1,178 @@
+﻿using LUSSISADTeam10API.Constants;
+using LUSSISADTeam10API.Models.APIModels;
+using LUSSISADTeam10API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LUSSISADTeam10API.Controllers
+{
+    [Authorize]
+    public class InventoryTransactionController : ApiController
+    {
+        // to show inventorytransaction list
+        [HttpGet]
+        [Route("api/inventorytransactions")]
+        public IHttpActionResult GetAllInventoryTransactions()
+        {
+            // declare and initialize error variable to accept the error from Repo
+            string error = "";
+
+            // get the list from inventorytransactionrepo and will insert the error if there is one
+            List<InventoryTransactionModel> invtms = InventoryTransactionRepo.GetAllInventoryTransactions(out error);
+
+            // if the erorr is not blank or the inventorytransaction list is null
+            if (error != "" || invtms == null)
+            {
+                // if the error is 404
+                if (error == ConError.Status.NOTFOUND)
+                    return Content(HttpStatusCode.NotFound, "InventoryTransactions Not Found");
+                // if the error is other one
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            // if there is no error
+            return Ok(invtms);
+
+        }
+
+        // to get inventorytransaction by inventorytransaction id
+        [HttpGet]
+        [Route("api/inventorytransaction/{invtid}")]
+        public IHttpActionResult GetInventoryTransactionByInvtID(int invtid)
+        {
+            string error = "";
+            InventoryTransactionModel invtm = InventoryTransactionRepo.GetInventoryTransactionByTransID(invtid, out error);
+            if (error != "" || invtm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtm);
+        }
+
+        // to get inventorytransaction by inventory id
+        [HttpGet]
+        [Route("api/inventorytransaction/inventory/{invid}")]
+        public IHttpActionResult GetInventoryTransactionByInvID(int invid)
+        {
+            string error = "";
+            List<InventoryTransactionModel> invtms = InventoryTransactionRepo.GetInventoryTransactionsByInvID(invid, out error);
+            if (error != "" || invtms == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtms);
+        }
+
+        [HttpGet]
+        [Route("api/inventorytransaction/item/{itemid}")]
+        public IHttpActionResult GetInventoryTransactionByItemID(int itemid)
+        {
+            string error = "";
+            List<InventoryTransactionModel> invtms = InventoryTransactionRepo.GetInventoryTransactionsByItemID(itemid, out error);
+            if (error != "" || invtms == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtms);
+        }
+
+        [HttpGet]
+        [Route("api/inventorytransaction/transtype/{transtype}")]
+        public IHttpActionResult GetInventoryTransactionByTransType(int transtype)
+        {
+            string error = "";
+            List<InventoryTransactionModel> invtms = InventoryTransactionRepo.GetInventoryTransactionsByTransType(transtype, out error);
+            if (error != "" || invtms == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtms);
+        }
+
+        [HttpGet]
+        [Route("api/inventorytransaction/transdate/{transdate}")]
+        public IHttpActionResult GetInventoryTransactionByTransDate(DateTime transdate)
+        {
+            string error = "";
+            List<InventoryTransactionModel> invtms = InventoryTransactionRepo.GetInventoryTransactionsByTransDate(transdate, out error);
+            if (error != "" || invtms == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtms);
+        }
+
+        [HttpGet]
+        [Route("api/inventorytransaction/transdaterange/{startdate}/{enddate}")]
+        public IHttpActionResult GetInventoryTransactionByTransDate(DateTime startdate, DateTime enddate)
+        {
+            string error = "";
+            List<InventoryTransactionModel> invtms = InventoryTransactionRepo.GetInventoryTransactionsByTransDateRange(startdate, enddate, out error);
+            if (error != "" || invtms == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtms);
+        }
+
+        // to update inventorytransaction
+        [HttpPost]
+        [Route("api/inventorytransaction/update")]
+        public IHttpActionResult UpdateInventoryTransaction(InventoryTransactionModel invt)
+        {
+            string error = "";
+            InventoryTransactionModel invtm = InventoryTransactionRepo.UpdateInventoryTransaction(invt, out error);
+            if (error != "" || invtm == null)
+            {
+                if (error == ConError.Status.NOTFOUND)
+                {
+                    return Content(HttpStatusCode.NotFound, "InventoryTransaction Not Found");
+                }
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtm);
+        }
+
+        // to create new inventorytransaction
+        [HttpPost]
+        [Route("api/inventorytransaction/create")]
+        public IHttpActionResult CreateInventoryTransaction(InventoryTransactionModel invt)
+        {
+            string error = "";
+            InventoryTransactionModel invtm = InventoryTransactionRepo.CreateInventoryTransaction(invt, out error);
+            if (error != "" || invtm == null)
+            {
+                return Content(HttpStatusCode.BadRequest, error);
+            }
+            return Ok(invtm);
+        }
+
+    }
+}
---
+++ LUSSISADTeam10API/Repositories/InventoryTransactionRepo.cs
@@ -0,0 +1,263 @@
+﻿using LUSSISADTeam10API.Constants;
+using LUSSISADTeam10API.Models.APIModels;
+using LUSSISADTeam10API.Models.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSISADTeam10API.Repositories
+{
+    public class InventoryTransactionRepo
+    {
+        // Convert From Auto Generated DB Model to APIModel
+        private static InventoryTransactionModel ConvertDBModeltoAPIModel(inventorytransaction invt)
+        {
+            InventoryTransactionModel invtm = new InventoryTransactionModel(invt.tranid, invt.datetime, invt.invid, invt.itemid, invt.item.description, invt.item.uom, invt.item.category.name, invt.trantype, invt.qty, invt.remark);
+            return invtm;
+        }
+        public static List<InventoryTransactionModel> GetAllInventoryTransactions(out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            // Initializing the error variable to return only blank if there is no error
+            error = "";
+            List<InventoryTransactionModel> invtms = new List<InventoryTransactionModel>();
+            try
+            {
+                // get inventorytransaction list from database
+                List<inventorytransaction> invts = entities.inventorytransactions.ToList<inventorytransaction>();
+
+                // convert the DB Model list to API Model list
+                foreach (inventorytransaction invt in invts)
+                {
+                    invtms.Add(ConvertDBModeltoAPIModel(invt));
+                }
+            }
+
+            // if inventorytransaction not found, will throw NOTFOUND exception
+            catch (NullReferenceException)
+            {
+                // if there is NULL Exception error, error will be 404
+                error = ConError.Status.NOTFOUND;
+            }
+
+            catch (Exception e)
+            {
+                // for other exceptions
+                error = e.Message;
+            }
+
+            //returning the list
+            return invtms;
+        }
+        public static InventoryTransactionModel GetInventoryTransactionByTransID(int transid, out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            error = "";
+
+            inventorytransaction invt = new inventorytransaction();
+            InventoryTransactionModel invtm = new InventoryTransactionModel();
+            try
+            {
+                invt = entities.inventorytransactions.Where(p => p.tranid == transid).FirstOrDefault<inventorytransaction>();
+                invtm = ConvertDBModeltoAPIModel(invt);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtm;
+        }
+        public static List<InventoryTransactionModel> GetInventoryTransactionsByInvID(int invid, out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            error = "";
+
+            List<inventorytransaction> invts = new List<inventorytransaction>();
+            List<InventoryTransactionModel> invtms = new List<InventoryTransactionModel>();
+            try
+            {
+                invts = entities.inventorytransactions.Where(p => p.invid == invid).ToList<inventorytransaction>();
+                foreach(inventorytransaction invt in invts)
+                {
+                    invtms.Add(ConvertDBModeltoAPIModel(invt));
+                }
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtms;
+        }
+        public static List<InventoryTransactionModel> GetInventoryTransactionsByItemID(int itemid, out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            error = "";
+
+            List<inventorytransaction> invts = new List<inventorytransaction>();
+            List<InventoryTransactionModel> invtms = new List<InventoryTransactionModel>();
+            try
+            {
+                invts = entities.inventorytransactions.Where(p => p.itemid == itemid).ToList<inventorytransaction>();
+                foreach (inventorytransaction invt in invts)
+                {
+                    invtms.Add(ConvertDBModeltoAPIModel(invt));
+                }
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtms;
+        }
+        public static List<InventoryTransactionModel> GetInventoryTransactionsByTransType(int transtype, out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            error = "";
+
+            List<inventorytransaction> invts = new List<inventorytransaction>();
+            List<InventoryTransactionModel> invtms = new List<InventoryTransactionModel>();
+            try
+            {
+                invts = entities.inventorytransactions.Where(p => p.trantype == transtype).ToList<inventorytransaction>();
+                foreach (inventorytransaction invt in invts)
+                {
+                    invtms.Add(ConvertDBModeltoAPIModel(invt));
+                }
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtms;
+        }
+        public static List<InventoryTransactionModel> GetInventoryTransactionsByTransDate(DateTime date, out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            error = "";
+
+            List<inventorytransaction> invts = new List<inventorytransaction>();
+            List<InventoryTransactionModel> invtms = new List<InventoryTransactionModel>();
+            try
+            {
+                invts = entities.inventorytransactions.Where(p => p.datetime.Date == date.Date).ToList<inventorytransaction>();
+                foreach (inventorytransaction invt in invts)
+                {
+                    invtms.Add(ConvertDBModeltoAPIModel(invt));
+                }
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtms;
+        }
+        public static List<InventoryTransactionModel> GetInventoryTransactionsByTransDateRange(DateTime startdate, DateTime enddate, out string error)
+        {
+            LUSSISEntities entities = new LUSSISEntities();
+            error = "";
+
+            List<inventorytransaction> invts = new List<inventorytransaction>();
+            List<InventoryTransactionModel> invtms = new List<InventoryTransactionModel>();
+            try
+            {
+                invts = entities.inventorytransactions.Where(p => p.datetime.Date >= startdate.Date && p.datetime.Date <= enddate.Date).ToList<inventorytransaction>();
+                foreach (inventorytransaction invt in invts)
+                {
+                    invtms.Add(ConvertDBModeltoAPIModel(invt));
+                }
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtms;
+        }
+        public static InventoryTransactionModel UpdateInventoryTransaction(InventoryTransactionModel invtm, out string error)
+        {
+            error = "";
+            // declare and initialize new LUSSISEntities to perform update
+            LUSSISEntities entities = new LUSSISEntities();
+            inventorytransaction d = new inventorytransaction();
+            try
+            {
+                // finding the inventorytransaction object using InventoryTransaction API model
+                d = entities.inventorytransactions.Where(p => p.tranid == invtm.TranID).First<inventorytransaction>();
+
+                // transfering data from API model to DB Model
+                d.datetime = DateTime.Now;
+                d.invid = invtm.InvID;
+                d.itemid = invtm.ItemID;
+                d.trantype = invtm.TransType;
+                d.qty = invtm.Qty;
+                d.remark = invtm.Remark;
+
+                // saving the update
+                entities.SaveChanges();
+
+                // return the updated model 
+                invtm = ConvertDBModeltoAPIModel(d);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtm;
+        }
+        public static InventoryTransactionModel CreateInventoryTransaction(InventoryTransactionModel invtm, out string error)
+        {
+            error = "";
+            LUSSISEntities entities = new LUSSISEntities();
+            inventorytransaction d = new inventorytransaction();
+            try
+            {
+                d.datetime = DateTime.Now;
+                d.invid = invtm.InvID;
+                d.itemid = invtm.ItemID;
+                d.trantype = invtm.TransType;
+                d.qty = invtm.Qty;
+                d.remark = invtm.Remark;
+                d = entities.inventorytransactions.Add(d);
+                entities.SaveChanges();
+                invtm = ConvertDBModeltoAPIModel(d);
+            }
+            catch (NullReferenceException)
+            {
+                error = ConError.Status.NOTFOUND;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return invtm;
+        }
+    }
+}
