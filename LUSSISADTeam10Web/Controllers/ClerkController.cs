9a53479aeafa1a876aede95fd46e468684e386bb
Khin Yadana Phyo <bakafool12@gmail.com>
2018-07-20 09:47:34 +0800

    Stationary Retrieval Form

--- LUSSISADTeam10Web/Controllers/ClerkController.cs
+++ LUSSISADTeam10Web/Controllers/ClerkController.cs
@@ -143,22 +143,38 @@ namespace LUSSISADTeam10Web.Controllers
             UserModel um = GetUser();
 
             List<OutstandingItemModel> inendetail = new List<OutstandingItemModel>();
+            List<BreakdownByDepartmentModel> bkm = new List<BreakdownByDepartmentModel>();
 
+            List<ShowBD> bkmd = new List<ShowBD>();
             try
             {
                 inendetail = APIDisbursement.GetRetriveItemListforClerk(token, out string error);
 
-                if (error != "")
+                bkm = APIDisbursement.GetBreakDown(token, out string errors);
+
+                foreach(BreakdownByDepartmentModel bd in bkm)
                 {
-                    return RedirectToAction("Index", "Error", new { error });
+                    ShowBD s = new ShowBD();
+
+                    s.ItemID = bd.ItemID;
+                    s.ItemDescription = bd.ItemDescription;
+                    s.Qty = inendetail.Where(x => x.ItemId == bd.ItemID).FirstOrDefault().Total;
+                    s.BDList = bd.BDList;
+
+                    bkmd.Add(s);
                 }
-            }
-            catch (Exception ex)
-            {
-                RedirectToAction("Index", "Error", new { error = ex.Message });
+                
             }
 
-            return View(inendetail);
+
+
+
+            catch
+            {
+
+            }
+
+            return View(bkmd);
         }
 
 
