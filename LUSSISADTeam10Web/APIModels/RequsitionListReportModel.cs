3fdfc76c4fc7e6c0cdb6813bd15e2190ddfb8648
Khin Yadana Phyo <bakafool12@gmail.com>
2018-07-23 09:42:33 +0800

    Hi

---
+++ LUSSISADTeam10Web/APIModels/RequsitionListReportModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSISADTeam10Web.Models.APIModels
+{
+    public class RequsitionListReportModel
+    {
+        public RequsitionListReportModel(DateTime? reqdate, string status, int deptid)
+        {
+
+            this.Reqdate = reqdate;
+            this.Status = status;
+            this.Deptid = deptid;
+        }
+        public DateTime? Reqdate { get; set; }
+
+        public string Status { get; set; }
+
+        public int Deptid { get; set; }
+
+
+
+
+
+    }
+}
