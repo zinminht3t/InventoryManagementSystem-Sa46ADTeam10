0c50144aff2159e6dd50db9fd33acfd2aba58045
Zin Min Htet <zinminht3t@gmail.com>
2018-07-26 18:13:21 +0800

    Change Logic

--- LUSSISADTeam10Web/Controllers/ClerkController.cs
+++ LUSSISADTeam10Web/Controllers/ClerkController.cs
@@ -915,8 +915,7 @@ namespace LUSSISADTeam10Web.Controllers
             reqm = APIRequisition.GetRequisitionByReqid(id, token, out error);
             outreqvm.CanFullFill = APIOutstandingReq.CheckInventoryStock(token, outr.OutReqId, out error);
 
-            if (reqm.Status != ConRequisition.Status.OUTSTANDINGREQUISITION ||
-                outr.Status != ConOutstandingsRequisition.Status.DELIVERED || outreqvm.CanFullFill == false)
+            if (reqm.Status != ConRequisition.Status.OUTSTANDINGREQUISITION ||outreqvm.CanFullFill == false)
             {
                 return RedirectToAction("Outstanding");
             }
