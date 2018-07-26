7c77d1aedf215463e79585c1d469bbd81289cc31
Aung Myo <aungmyo.am@u.nus.edu>
2018-07-26 11:53:33 +0800

    fix deelegation compltete

--- LUSSISADTeam10API/Repositories/DelegationRepo.cs
+++ LUSSISADTeam10API/Repositories/DelegationRepo.cs
@@ -187,7 +187,7 @@ namespace LUSSISADTeam10API.Repositories
                     {
                         delegation del = entities.delegations.Where(p => p.delid == deleg.Delid).FirstOrDefault<delegation>();
                         del.active = ConDelegation.Active.INACTIVE;
-                        u.Role = ConUser.Role.EMPLOYEEREP;
+                        UserRepo.canceldelegateuser(u.Userid);
                         entities.SaveChanges();
                     }
 
