679a09f9d0334c5d9a26968d8fd83962a61c005b
Khin Yadana Phyo <bakafool12@gmail.com>
2018-07-28 13:50:06 +0800

    Commit

---
+++ LUSSISADTeam10Web/Models/Report/RequisitionListViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSISADTeam10Web.Models.Report
+{
+    public class RequisitionListViewModel
+    {
+
+        public int deptid { get; set; }
+        public string status { get; set; }
+
+        public DateTime? reqdate { get; set; }
+
+        public DateTime startdate { get; set; }
+
+        public DateTime enddate { get; set; }
+
+        public List<RequisitionDetailViewModel> rd { get; set; }
+
+        
+    }
+
+    public class RequisitionDetailViewModel
+    {
+        public int deptid { get; set; }
+        public string status { get; set; }
+
+        public DateTime? reqdate { get; set; }
+
+        public DateTime startdate { get; set; }
+
+        public DateTime enddate { get; set; }
+
+    }
+}
