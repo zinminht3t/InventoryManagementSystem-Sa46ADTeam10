52ea52b6dac8840fd7c11c47cc895c55ba546603
Htet Wai Yan Htet Wai Yan <e0284026@u.nus.edu>
2018-07-29 16:05:43 +0800

    POReport

---
+++ LUSSISADTeam10Web/APIModels/POCountList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LUSSISADTeam10Web.Models.APIModels
+{
+    public class POCountList
+    {
+        public POCountList(string month, int total, int poCount)
+        {
+            Month = month;
+            Total = total;
+            PoCount = poCount;
+        }
+        public string Month { get; set; }
+        public int Total { get; set; }
+        public int PoCount { get; set; }
+    }
+}
